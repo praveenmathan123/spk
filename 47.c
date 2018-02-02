a3a28d14c0de89323f63e122b1d9cc7a99e9bdce
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-02 14:10:02 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int n,i,greatest,lowest;
+int a[];
+printf("enter the array size");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+greatest=0;lowest=0;
+for(i=0;i<n;i++)
+{
+if(a[i]>gratest)
+{
+greatest=a[i];
+}
+}
+for(i=n;i>0;i++)
+{
+if(a[i]<lowest)
+{
+  lowest=a[i];
+}  
+printf("%d",lowest);
+}
+return0;
+}
