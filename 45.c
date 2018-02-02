19912f7008cf663484037792662b4a2b0cac02d6
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-02 13:49:59 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+   int count=0;
+   long int n;
+   printf("enter the digits");
+   scanf("%ld",&n);
+   while(n!=0)
+   {
+   	count++;
+   	n=n/10;
+   }
+   printf("number of digit is:%ld",count);
+	return 0;
+}
+return0;
+}
