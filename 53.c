2c584df557e849988b8b51f5e813f4d963ba8c45
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-05 15:59:54 +0530

    Update 53.c

--- 53.c
+++ 53.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int a[],i;
+int a;
+sum=0;
+printf("enter the value");
+scanf("%d",&a);
+    while(a!=0)
+    {
+        m=a%10;
+        a=a/10;
+        sum=sum+m;
+    }
+    printf("%d",sum);
+    {
+i=n%10;
+    }
+  return0;
+}
