d09056a19443419de77591245780593d5865fb03
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-02-05 16:17:09 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+scanf("%d%d",a,b);
+printf("the unswap values are%d%d",a,b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("the swaped values%d%d",a,b);
+return 0;
+}
+return0;
+}
+}}
